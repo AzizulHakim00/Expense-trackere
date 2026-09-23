1c816db39f0279e54750143ef2fbcca557c320d8
Azizul Hakim Omor <157488702+AzizulHakim00@users.noreply.github.com>
2026-09-23 17:17:04 +0600

    Build Expense Tracker V1 core features

---
+++ src/main/java/com/expensetracker/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.expensetracker.repository;
+import com.expensetracker.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+public interface UserRepository extends MongoRepository<User,String> {
+    Optional<User> findByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCase(String email);
+}
